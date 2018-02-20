86b516fa5b1a3124ef58ddcf00783e9f72198864
Rozamo <shivamanand.murmu.cse16@itbhu.ac.in>
2018-02-20 14:18:01 +0530

    Submitting Assignment 3 for 16075049.

---
+++ Assignment 3/venv/Workspace.py
@@ -0,0 +1,25 @@
+import numpy as np
+# X_batch = np.random.random((3, 2))
+# print(X_batch)
+# # weights = np.vstack((np.random.random((X_batch.shape[1], 1)), np.ones((X_batch.shape[1], 1))))
+# # print(weights)
+# # print(np.matmul(X_batch, weights))
+# weights = np.vstack((np.ones((1, 1)), np.random.random((X_batch.shape[1], 1))))
+# X_batch = np.hstack((np.ones((X_batch.shape[0], 1)), X_batch))
+# print(weights)
+# print(X_batch)
+# print(np.matmul(X_batch, weights))
+
+# a = np.array([[1, 2], [3, 4]])
+# b = np.array([1, 2])
+# print(np.matmul(a, b))
+#weights = np.zeros((3))
+# weights = np.insert(np.random.random((3)), 0, 1)
+# print(weights)#np.vstack((np.ones((1)), np.random.random((3))))
+# def weights_func(w_sum):
+#     for w in range(1, 10):
+#         w_sum += w
+#     return w_sum
+# x = np.vectorize(weights_func)
+# print(x(0))
+
