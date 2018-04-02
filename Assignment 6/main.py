4214a9303c26e1468aab7924f8e1cdb66b5312f8
Shrey97 <shreytanna97@gmail.com>
2018-04-02 22:28:11 +0530

    Submitting Assignment 6 for 16075051.

--- Assignment 6/main.py
+++ Assignment 6/main.py
@@ -7,9 +7,9 @@ g = pickle.load(graphfile)
 bfs_list = list(g.bfs_paths(1, 8))
 dfs_list = list(g.dfs_paths(1, 11))
 print (len(dfs_list))
+print(len(bfs_list))
 bfs_pickle = open('bfs_result.pkl', 'wb')
 pickle.dump(bfs_list, bfs_pickle)
 
 dfs_pickle = open('dfs_result.pkl', 'wb')
 pickle.dump(dfs_list, dfs_pickle)
-
