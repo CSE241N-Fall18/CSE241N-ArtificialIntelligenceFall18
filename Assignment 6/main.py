8159bd2bc7ac7d40824df6e8eb3ff795d3a926f2
kamal <kamal.kalwa.cse16@itbhu.ac.in>
2018-04-03 02:03:31 +0530

    Submitting Assignment 6 for 16075032.

--- Assignment 6/main.py
+++ Assignment 6/main.py
@@ -6,7 +6,7 @@ graphfile = open('graphdata.pkl', 'rb')
 g = pickle.load(graphfile)
 bfs_list = list(g.bfs_paths(1, 8))
 dfs_list = list(g.dfs_paths(1, 11))
-print (len(bfs_list))
+print (len(dfs_list))
 bfs_pickle = open('bfs_result.pkl', 'wb')
 pickle.dump(bfs_list, bfs_pickle)
 
